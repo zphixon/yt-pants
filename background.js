18ec288849145a1774ef73e2be6f2d1edb1f53be
Zack <zphixon@gmail.com>
2022-04-17 14:41:14 -0400

    mostly nice behavior

--- background.js
+++ background.js
@@ -1,17 +1,24 @@
 // vim: et sts=2 ts=2 sw=2
 
+console.log('reload');
+
 const filter = {
   url: [
     {hostContains: "youtube.com"},
   ]
 };
 
+let updated = new Set();
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && changeInfo.url.includes('youtube.com/shorts/')) {
-    console.log('tab ' + tabId + ' navigated to shorts');
-    browser.tabs.update(tabId, {
-      url: changeInfo.url.replace(/shorts\//, 'watch?v='),
-    });
+  let url = changeInfo.url;
+  if (url && url.includes('youtube.com/shorts/')) {
+    if (!updated.has(url)) {
+      updated.add(url);
+      browser.tabs.update(tabId, {
+        url: url.replace(/shorts\//, 'watch?v='),
+      });
+    } else {
+      updated.delete(url);
+    }
   }
 });
-
