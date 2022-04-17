207167aa2fea613abcef3fa5528eb7f79c914a45
Zack <zphixon@gmail.com>
2022-04-17 16:34:27 -0400

    i hate javascript

--- background.js
+++ background.js
@@ -1,16 +1,55 @@
 // vim: et sts=2 ts=2 sw=2
 
-console.log('reload');
+function storeEnabled(newValue) {
+  //browser.storage.local.set({enabled: newValue});
+}
 
-const filter = {
-  url: [
-    {hostContains: "youtube.com"},
-  ]
-};
+function getEnabled() {
+  let result;
+  browser.storage.local.get('enabled', anObjectWithASingleKeyForSomeGodForsakenReason => {
+    if (anObjectWithASingleKeyForSomeGodForsakenReason == undefined) {
+      result = true;
+      storeEnabled(true);
+    } else {
+      result = anObjectWithASingleKeyForSomeGodForsakenReason.enabled;
+    }
+  });
+  return result;
+}
+
+getEnabled().then(fuck => {
+  console.log(fuck);
+});
+//console.log(getEnabled());
+
+let enabled = true;
+let storageValue = getEnabled();
+if (storageValue == 'not_set') {
+  storeEnabled(true);
+} else if (storageValue == 'disabled') {
+  enabled = false;
+}
+
+browser.storage.local.get('enabled').then(storage => {
+  if (storage.enabled != undefined) {
+    // if there is an enabled key on the storage object, set our local enabled
+    // state to it
+    enabled = storage.enabled;
+  } else {
+    // otherwise, set it to true here and in storage
+    enabled = true;
+    browser.storage.local.set({enabled});
+  }
+});
+
+// listen from the popup page for checkbox events
+browser.runtime.onMessage.addListener(message => {
+  enabled = message.checked;
+  browser.storage.local.set({enabled});
+});
 
 let updated = new Set();
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  let url = changeInfo.url;
+function changeUrl(url, tabId) {
   if (url && url.includes('youtube.com/shorts/')) {
     if (!updated.has(url)) {
       updated.add(url);
@@ -20,5 +59,17 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     } else {
       updated.delete(url);
     }
+  }
+}
+
+const filter = {
+  url: [
+    {hostContains: "youtube.com"},
+  ]
+};
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (enabled) {
+    changeUrl(changeInfo.url, tabId);
   }
 });
